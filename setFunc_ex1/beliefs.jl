bb4ce958cd868ce072ad013470a77baf0acf6489
Sam Akinwande <79815885+sakinwande@users.noreply.github.com>
2025-05-31 09:45:09 -0700

    Fix belief update and reward computation

--- setFunc_ex1/beliefs.jl
+++ setFunc_ex1/beliefs.jl
@@ -12,24 +12,33 @@ function Base.rand(rng::AbstractRNG, pomdp::setFuncPOMDP, b::setFuncBelief)
 end
 
 #TODO: Update observation to be a set of samples from a simplex
-function POMDPs.update(updater::setFuncBeliefUpdater, b::setFuncBelief, a::Int, o::Float64)
-    ub = update_belief(updater.pomdp, b, a, o, updater.pomdp.rng)
-    return ub
+"""
+    POMDPs.update(updater::setFuncBeliefUpdater, b::setFuncBelief, a::Symbol, o)
+
+Update the belief given a symbolic action and an observation.  The solver
+produces actions as `Symbol`s, so we convert to an action index and then call
+`update_belief` which performs the Gaussian process posterior update.
+"""
+function POMDPs.update(updater::setFuncBeliefUpdater, b::setFuncBelief, a::Symbol, o)
+    a_idx = POMDPs.actionindex(updater.pomdp, a)
+    return update_belief(updater.pomdp, b, a_idx, o, updater.pomdp.rng)
 end
 
 #TODO:
-function update_belief(pomdp::P, b::setFuncBelief, a::Int, o::Float64, rng::RNG) where {P <: POMDPs.POMDP, RNG <: AbstractRNG}
-    # NOTE: σ_n is the stddev whereas σ²_n is the varaiance. Julia uses σ_n
-    # for normal dist whereas our GP setup uses σ²_n
+function update_belief(pomdp::P, b::setFuncBelief, a::Int, o, rng::RNG) where {P <: POMDPs.POMDP, RNG <: AbstractRNG}
+    # `o` is a vector of the form [pos, observation]
+    pos_coord = o[1]
+    obs_val = o[2]
 
-    # if we sample we fully collapse the belief
-    σ²_n = pomdp.σ_drill #^2 dont square this causes singular exception in GP update
-    lb_posterior = posterior(b.lb_belief, [sample_pos[1]], [o[2]], [σ²_n])
-    ub_posterior = posterior(b.ub_belief, [sample_pos[1]], [o[2]], [σ²_n])
+    σ²_n = pomdp.σ_drill
 
-    new_eval_samples = push!(b.sampled_points, o[2])
-    sample_pos = convert_pos_coord_2_pos_idx(pomdp, sample_pos)
+    lb_posterior = posterior(b.lb_belief, [pos_coord[1]], [obs_val], [σ²_n])
+    ub_posterior = posterior(b.ub_belief, [pos_coord[1]], [obs_val], [σ²_n])
 
+    new_eval_samples = copy(b.sampled_points)
+    push!(new_eval_samples, obs_val)
+
+    sample_pos = convert_pos_coord_2_pos_idx(pomdp, pos_coord)
     return setFuncBelief(sample_pos, lb_posterior, ub_posterior, new_eval_samples)
 end
 
